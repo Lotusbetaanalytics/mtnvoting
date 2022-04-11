b2f3e84158c5d98239b7e7825373a3cdbd5961fc
iCode <gbengaobafemi1@gmail.com>
2022-04-11 18:17:30 +0100

    first commit

--- src/webparts/mtnvoting/components/Mtnvoting.tsx
+++ src/webparts/mtnvoting/components/Mtnvoting.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import styles from './Mtnvoting.module.scss';
 import { IMtnvotingProps } from './IMtnvotingProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { Route, Switch, HashRouter } from 'react-router-dom';
@@ -8,18 +7,17 @@ import "@pnp/sp/webs";
 import "@pnp/sp/site-users/web";
 import * as pnp from 'sp-pnp-js';
 import { AdminDashboard, ErrorScreen } from './screens';
+import "./global.scss"
+
 
 export default class Mtnvoting extends React.Component<IMtnvotingProps, {}> {
   public render(): React.ReactElement<IMtnvotingProps> {
+    jQuery("#workbenchPageContent").prop("style", "max-width: none"); jQuery(".SPCanvas-canvas").prop("style", "max-width: none"); jQuery(".CanvasZone").prop("style", "max-width: none");
     return (
       <HashRouter>
         <Switch>
           <Route path="/admin" exact component={AdminDashboard} />
-
-
-
           <Route component={ErrorScreen} />
-
         </Switch>
       </HashRouter>
     );
--- src/webparts/mtnvoting/components/screens/Error/index.tsx
+++ src/webparts/mtnvoting/components/screens/Error/index.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react'
-
+import { Link } from 'react-router-dom'
+import styles from './styles.module.scss'
 const ErrorScreen = () => {
     return (
-        <div>Error</div>
+        <div className={styles.errorScreen}>
+            <h1>Error 404</h1>
+            <h2>Page Not Found</h2>
+            <div className='minimizeBtn'>
+                <Link to={'/'} className="mtn__btn mtn__yellow">Go Back</Link>
+            </div>
+
+        </div>
     )
 }
 
