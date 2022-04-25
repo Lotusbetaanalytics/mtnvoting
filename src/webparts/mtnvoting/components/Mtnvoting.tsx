d661280adc68d9d0f0c5a1f6b0f8cf8fea10420c
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2022-04-25 11:11:28 +0100

    mtn voting page

--- src/webparts/mtnvoting/components/Mtnvoting.tsx
+++ src/webparts/mtnvoting/components/Mtnvoting.tsx
@@ -1,31 +1,64 @@
-import * as React from 'react';
-import { IMtnvotingProps } from './IMtnvotingProps';
-import { escape } from '@microsoft/sp-lodash-subset';
-import { Route, Switch, HashRouter } from 'react-router-dom';
-import * as jQuery from 'jquery';
+import * as React from "react";
+import { IMtnvotingProps } from "./IMtnvotingProps";
+import { escape } from "@microsoft/sp-lodash-subset";
+import { Route, Switch, HashRouter } from "react-router-dom";
+import * as jQuery from "jquery";
 import "@pnp/sp/webs";
 import "@pnp/sp/site-users/web";
-import * as pnp from 'sp-pnp-js';
-import { AdminApproved, AdminDashboard, AdminDeclined, AdminPending, AdminViewApproved, AdminViewPending, ErrorScreen } from './screens';
-import "./global.scss"
-import './assets/icon.scss'
-
+import * as pnp from "sp-pnp-js";
+import {
+  AdminApproved,
+  AdminDashboard,
+  AdminDeclined,
+  AdminPending,
+  AdminViewApproved,
+  AdminViewPending,
+  ErrorScreen,
+  VotingPage,
+} from "./screens";
+import "./global.scss";
+import "./assets/icon.scss";
 
 export default class Mtnvoting extends React.Component<IMtnvotingProps, {}> {
   public render(): React.ReactElement<IMtnvotingProps> {
-    jQuery("#workbenchPageContent").prop("style", "max-width: none"); jQuery(".SPCanvas-canvas").prop("style", "max-width: none"); jQuery(".CanvasZone").prop("style", "max-width: none");
+    jQuery("#workbenchPageContent").prop("style", "max-width: none");
+    jQuery(".SPCanvas-canvas").prop("style", "max-width: none");
+    jQuery(".CanvasZone").prop("style", "max-width: none");
     return (
-      <HashRouter>
-        <Switch>
-          <Route path="/admin" exact component={AdminDashboard} />
-          <Route path="/admin/pending" exact component={AdminPending} />
-          <Route path="/admin/pending/:id" exact component={AdminViewPending} />
-          <Route path="/admin/approved" exact component={AdminApproved} />
-          <Route path="/admin/approved/:id" exact component={AdminViewApproved} />
-          <Route path="/admin/declined" exact component={AdminDeclined} />
-          <Route component={ErrorScreen} />
-        </Switch>
-      </HashRouter>
+      <Context.Provider
+        value={{
+          spHttpClient: this.props.context.spHttpClient,
+        }}
+      >
+        <HashRouter>
+          <Switch>
+            <Route path="/admin" exact component={AdminDashboard} />
+            <Route path="/admin/pending" exact component={AdminPending} />
+            <Route
+              path="/admin/pending/:id"
+              exact
+              component={AdminViewPending}
+            />
+            <Route path="/admin/approved" exact component={AdminApproved} />
+            <Route
+              path="/admin/approved/:id"
+              exact
+              component={AdminViewApproved}
+            />
+            <Route path="/admin/declined" exact component={AdminDeclined} />
+
+            {/* Voting Page */}
+            <Route path="/voting" exact component={VotingPage} />
+
+            {/* Page Not found */}
+            <Route component={ErrorScreen} />
+          </Switch>
+        </HashRouter>
+      </Context.Provider>
     );
   }
 }
+
+export const Context = React.createContext({
+  spHttpClient: null,
+});
--- src/webparts/mtnvoting/components/screens/Employee/Voting Page/index.tsx
+++ src/webparts/mtnvoting/components/screens/Employee/Voting Page/index.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
-import { Header, NomineeCard } from "../../../containers";
+import { Header, Modal, NomineeCard } from "../../../containers";
 import { sp } from "@pnp/sp";
 import styles from "./voting.module.scss";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import Carousel from "react-elastic-carousel";
 import {
   SPHttpClient,
   SPHttpClientConfiguration,
   SPHttpClientResponse,
 } from "@microsoft/sp-http";
 import { Context } from "../../../Mtnvoting";
+import swal from "sweetalert";
+import { useHistory } from "react-router-dom";
 
 const index = () => {
   const [nominees, setNominees] = React.useState([]);
@@ -16,101 +19,150 @@ const index = () => {
   const [indexFrom, setIndexFrom] = React.useState(0);
   const [indexTo, setIndexTo] = React.useState(4);
   const { spHttpClient } = React.useContext(Context);
+  const [open, setOpen] = React.useState(false);
+  const [id, setId] = React.useState();
+  const [userEmail, setUserEmail] = React.useState();
+  const [userID, setUserID] = React.useState();
 
-  const onNext = () => {
-    setIndexTo((prev) => prev + 4);
-    setIndexFrom((prev) => prev + 4);
-  };
-  const onPrev = () => {
-    setIndexTo((prev) => prev - 4);
-    setIndexFrom((prev) => prev - 4);
-  };
-
-  React.useEffect(() => {
-    spHttpClient
-      .get(
-        `https://lotusbetaanalytics.sharepoint.com/sites/business_solutions/_api/lists/GetByTitle('CURRENT HCM STAFF LIST-test')/items`,
-        SPHttpClient.configurations.v1
-      )
-      .then((response: SPHttpClientResponse) => {
-        response.json().then((responseJSON: any) => {
-          console.log(responseJSON.value);
-        });
-      });
-  }, []);
+  const history = useHistory();
 
   //Get all Nominees
   React.useEffect(() => {
     sp.web.lists
       .getByTitle(`Nominees`)
-      .items.skip(indexFrom)
-      .top(indexTo)
-      .filter(`Status eq 'Approved'`)
+      .items.filter(`Status eq 'Approved'`)
       .get()
       .then((res) => {
         console.log(res);
         setNominees(res);
       });
   }, []);
-  //Get all Employees
+
+  //find the logged in user and check if the user already voted
   React.useEffect(() => {
-    sp.web.lists
-      .getByTitle(`Registration`)
-      .items.get()
-      .then((res) => {
-        console.log(res);
-      });
+    sp.profiles.myProperties.get().then((res) => {
+      setUserEmail(res.Email);
+      sp.web.lists
+        .getByTitle("Registration")
+        .items.filter(`EmployeeEmail eq '${res.Email}'`)
+        .get()
+        .then((items) => {
+          setUserID(items[0].ID);
+          if (items[0].Voted) {
+            swal({
+              closeOnClickOutside: false,
+              closeOnEsc: false,
+              text: "You have voted already!",
+            }).then(() => {
+              history.push("/");
+            });
+          }
+        });
+    });
   }, []);
 
   const votedNominee = (id) => {
+    setId(id);
+    setOpen(true);
+  };
+
+  //Find a nominee with their id
+  const addNomineeVotes = (id) => {
+    return nominees.filter((nominee) => nominee.ID === id);
+  };
+
+  const yesHandler = () => {
     sp.web.lists
-      .getByTitle(`Nominees`)
-      .items.getById(id)
-      .update({
-        Status: "Voted",
+      .getByTitle(`Votes`)
+      .items.add({
+        EmployeeID: userID,
+        Nominee: id,
       })
       .then(() => {
         sp.web.lists
           .getByTitle(`Nominees`)
-          .items.filter(`Status eq 'Approved'`)
-          .get()
+          .items.get()
           .then((res) => {
             setNominees(res);
+          });
+
+        sp.web.lists
+          .getByTitle(`Registration`)
+          .items.getById(userID)
+          .update({
+            Voted: true,
+          })
+          .then(() => {
+            setTimeout(() => {
+              setOpen(false);
+            }, 1000);
           });
       });
   };
 
+  const noHandler = () => {
+    setOpen(false);
+  };
+
+  const votePermissions = () => {
+    return (
+      <div className={styles.modalContent}>
+        <span>Are you sure you want to vote for this candidate?</span>
+        <div className={styles.modalContentButton}>
+          <button onClick={noHandler}>No</button>
+          <button onClick={yesHandler}>Yes</button>
+        </div>
+      </div>
+    );
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
+
   return (
     <div className={styles.votingPageContainer}>
       <Header title="Nominees" />
-      {indexFrom >= 4 && (
-        <div className={styles.arrowLeft} onClick={onPrev}>
-          <AiOutlineArrowLeft />
-        </div>
-      )}
+
       <div className={styles.nomineeContainerScreen}>
-        {nominees.map((nominee) => {
-          return (
-            <div>
-              <NomineeCard
-                checked={checked}
-                image={nominee.image}
-                name={nominee.EmployeeName}
-                lastName={nominee.lastName}
-                onClick={() => {
-                  votedNominee(nominee.id);
-                }}
-              />
-            </div>
-          );
-        })}
+        <Carousel
+          breakPoints={breakPoints}
+          isRTL={false}
+          initialActiveIndex={0}
+          pagination={false}
+          className={styles.carousel}
+        >
+          {nominees.map((nominee) => {
+            return (
+              <>
+                <NomineeCard
+                  checked={checked}
+                  image={nominee.PassportPhotograph}
+                  name={nominee.EmployeeName}
+                  lastName={nominee.lastName}
+                  onClick={() => {
+                    votedNominee(nominee.ID);
+                  }}
+                />
+              </>
+            );
+          })}
+        </Carousel>
       </div>
 
-      {indexTo > 4 && (
-        <div className={styles.arrowRight} onClick={onNext}>
-          <AiOutlineArrowRight />
-        </div>
-      )}
+      <Modal
+        content={votePermissions()}
+        onClose={handleClose}
+        isVisible={open}
+        size="sm"
+      />
     </div>
   );
 };
